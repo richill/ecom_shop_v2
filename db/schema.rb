a63d621389f35856a0e683c13e2abf13375ded6d
richill <richill.tamakloe@gmail.com>
2023-12-07 16:59:46 +0000

    Update content

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_04_215051) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_07_165326) do
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "favorited_type", null: false
+    t.integer "favorited_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["favorited_type", "favorited_id"], name: "index_favorites_on_favorited"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.string "image"
@@ -43,4 +53,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_04_215051) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "favorites", "users"
 end
