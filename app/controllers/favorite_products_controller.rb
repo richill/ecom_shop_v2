4d59a935491066ac698223205d9317ebd716d18c
richill <richill.tamakloe@gmail.com>
2023-12-07 17:31:48 +0000

    Update content

--- app/controllers/favorite_products_controller.rb
+++ app/controllers/favorite_products_controller.rb
@@ -1,7 +1,21 @@
 class FavoriteProductsController < ApplicationController
+  before_action :set_product
+
   def create
+    if Favorite.create(favorited: @product, user: current_user)
+      redirect_to @product, notice: 'Product has been favorited'
+    else
+      redirect_to @product, alert: 'Something went wrong...*sad panda*'
+    end
   end
 
   def destroy
+    Favorite.where(favorited_id: @product.id, user_id: current_user.id).first.destroy
+    redirect_to @product, notice: 'Product is no longer in favorites'
+  end
+
+  private
+  def set_product
+    @product = Product.find(params[:product_id] || params[:id])
   end
 end
