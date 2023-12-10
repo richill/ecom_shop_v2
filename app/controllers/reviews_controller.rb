bf8a88db7405c4dd95a29bb9877359d3621e8a05
richill <richill.tamakloe@gmail.com>
2023-12-10 15:44:30 +0000

    Update content

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -26,7 +26,7 @@ class ReviewsController < ApplicationController
 
     respond_to do |format|
       if @review.save
-        format.html { redirect_to review_url(@review), notice: "Review was successfully created." }
+        format.html { redirect_to review_url(@review, product_id: @review.product), notice: "Review was successfully created." }
         format.json { render :show, status: :created, location: @review }
       else
         format.html { render :new, status: :unprocessable_entity }
