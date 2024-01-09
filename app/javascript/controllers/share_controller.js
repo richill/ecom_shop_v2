fe8d49cb172a40d307b0c59cff4017795d04e766
richill <richill.tamakloe@gmail.com>
2024-01-09 12:27:30 +0000

    Update share code

--- app/javascript/controllers/share_controller.js
+++ app/javascript/controllers/share_controller.js
@@ -2,10 +2,20 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="share"
 export default class extends Controller {
-  static targets = ["result"]
+  static targets = ["result", "name", "price"];
   connect() {
     console.log('connected to the share controller');
     console.log(this.data.get("urlValue"));
-    console.log(this.data.resultTarget);
+    console.log(this.resultTarget);
+  }
+  async share(e) {
+    e.preventDefault();
+
+    const shareData = {
+      name: this.nameTarget.value,
+      price: this.priceTarget.value,
+      url: this.data.get("urlValue"),
+    };
+    console.log(shareData)
   }
 }
