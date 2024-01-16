a6dc86784ee79933e33b6617d2f197533d84594e
richill <richill.tamakloe@gmail.com>
2024-01-16 16:07:40 +0000

    Update content

--- db/seeds.rb
+++ db/seeds.rb
@@ -53,56 +53,56 @@ cps16 = CategoryProductsubtype.create!(name: 'shoes accessories', code_name: 'p_
 # level_04
 CategoryProductstyle.delete_all
 # accessories/belts
-cpst01 = CategoryProductstyle.create!(name: 'buckle', code_name: 'p_style_buckle', category_productsubtype_id: 0, admin_id: 1)
-cpst02 = CategoryProductstyle.create!(name: 'non buckle', code_name: 'p_style_non_buckle', category_productsubtype_id: 0, admin_id: 1)
+cpst01 = CategoryProductstyle.create!(name: 'buckle', code_name: 'p_style_buckle', category_productsubtype_id: cps01, user_id: us01)
+cpst02 = CategoryProductstyle.create!(name: 'non buckle', code_name: 'p_style_non_buckle', category_productsubtype_id: cps01, user_id: us01)
 # accessories/gloves
-cpst03 = CategoryProductstyle.create!(name: 'knitted', code_name: 'p_style_knitted', category_productsubtype_id: 0, admin_id: 1)
-cpst04 = CategoryProductstyle.create!(name: 'fingerless', code_name: 'p_style_fingerless', category_productsubtype_id: 0, admin_id: 1)
+cpst03 = CategoryProductstyle.create!(name: 'knitted', code_name: 'p_style_knitted', category_productsubtype_id: cps02, user_id: us01)
+cpst04 = CategoryProductstyle.create!(name: 'fingerless', code_name: 'p_style_fingerless', category_productsubtype_id: cps02, user_id: us01)
 # accessories/hats & caps
-cpst05 = CategoryProductstyle.create!(name: 'hats', code_name: 'p_style_mitten', category_productsubtype_id: 0, admin_id: 1)
-cpst06 = CategoryProductstyle.create!(name: 'caps', code_name: 'p_style_caps', category_productsubtype_id: 0, admin_id: 1)
+cpst05 = CategoryProductstyle.create!(name: 'hats', code_name: 'p_style_mitten', category_productsubtype_id: cps03, user_id: us01)
+cpst06 = CategoryProductstyle.create!(name: 'caps', code_name: 'p_style_caps', category_productsubtype_id: cps03, user_id: us01)
 # accessories/hair accessories
-cpst07 = CategoryProductstyle.create!(name: 'headbands', code_name: 'p_style_headbands', category_productsubtype_id: 0, admin_id: 1)
-cpst08 = CategoryProductstyle.create!(name: 'hair clips', code_name: 'p_style_hair_clips', category_productsubtype_id: 0, admin_id: 1)
+cpst07 = CategoryProductstyle.create!(name: 'headbands', code_name: 'p_style_headbands', category_productsubtype_id: cps04, user_id: us01)
+cpst08 = CategoryProductstyle.create!(name: 'hair clips', code_name: 'p_style_hair_clips', category_productsubtype_id: cps04, user_id: us01)
 # --------------------------------------
 # coats & jackets/jackets
-cpst09 = CategoryProductstyle.create!(name: 'puffer jacket', code_name: 'p_style_puffer_jacket', category_productsubtype_id: 0, admin_id: 1)
-cpst10 = CategoryProductstyle.create!(name: 'parkas', code_name: 'p_style_parkas', category_productsubtype_id: 0, admin_id: 1)
+cpst09 = CategoryProductstyle.create!(name: 'puffer jacket', code_name: 'p_style_puffer_jacket', category_productsubtype_id: cps05, user_id: us01)
+cpst10 = CategoryProductstyle.create!(name: 'parkas', code_name: 'p_style_parkas', category_productsubtype_id: cps05, user_id: us01)
 # coats & jackets/coats
-cpst11 = CategoryProductstyle.create!(name: 'puffer coats', code_name: 'p_style_puffer_coats', category_productsubtype_id: 0, admin_id: 1)
-cpst12 = CategoryProductstyle.create!(name: 'tweed coats', code_name: 'p_style_tweed_coats', category_productsubtype_id: 0, admin_id: 1)
+cpst11 = CategoryProductstyle.create!(name: 'puffer coats', code_name: 'p_style_puffer_coats', category_productsubtype_id: cps06, user_id: us01)
+cpst12 = CategoryProductstyle.create!(name: 'tweed coats', code_name: 'p_style_tweed_coats', category_productsubtype_id: cps06, user_id: us01)
 # coats & jackets/capes
-cpst13 = CategoryProductstyle.create!(name: 'ponchos', code_name: 'p_style_ponchos', category_productsubtype_id: 0, admin_id: 1)
-cpst14 = CategoryProductstyle.create!(name: 'kimonos', code_name: 'p_style_kimonos', category_productsubtype_id: 0, admin_id: 1)
+cpst13 = CategoryProductstyle.create!(name: 'ponchos', code_name: 'p_style_ponchos', category_productsubtype_id: cps07, user_id: us01)
+cpst14 = CategoryProductstyle.create!(name: 'kimonos', code_name: 'p_style_kimonos', category_productsubtype_id: cps07, user_id: us01)
 # coats & jackets/gilet
-cpst15 = CategoryProductstyle.create!(name: 'puffer', code_name: 'p_style_puffer', category_productsubtype_id: 0, admin_id: 1)
-cpst16 = CategoryProductstyle.create!(name: 'teddy', code_name: 'p_style_teddy', category_productsubtype_id: 0, admin_id: 1)
+cpst15 = CategoryProductstyle.create!(name: 'puffer', code_name: 'p_style_puffer', category_productsubtype_id: cps08, user_id: us01)
+cpst16 = CategoryProductstyle.create!(name: 'teddy', code_name: 'p_style_teddy', category_productsubtype_id: cps08, user_id: us01)
 # --------------------------------------
 # clothing/dresses
-cpst17 = CategoryProductstyle.create!(name: 'occasion dress', code_name: 'p_style_occasion_dress', category_productsubtype_id: 0, admin_id: 1)
-cpst18 = CategoryProductstyle.create!(name: 'casual dress', code_name: 'p_style_casual_dress', category_productsubtype_id: 0, admin_id: 1)
+cpst17 = CategoryProductstyle.create!(name: 'occasion dress', code_name: 'p_style_occasion_dress', category_productsubtype_id: cps09, user_id: us01)
+cpst18 = CategoryProductstyle.create!(name: 'casual dress', code_name: 'p_style_casual_dress', category_productsubtype_id: cps09, user_id: us01)
 # clothing/tops & tees
-cpst19 = CategoryProductstyle.create!(name: 'shirts', code_name: 'p_style_shirts', category_productsubtype_id: 0, admin_id: 1)
-cpst20 = CategoryProductstyle.create!(name: 'tuxedo shirts', code_name: 'p_style_tuxedo_shirts', category_productsubtype_id: 0, admin_id: 1)
+cpst19 = CategoryProductstyle.create!(name: 'shirts', code_name: 'p_style_shirts', category_productsubtype_id: cps10, admin_id: 1)
+cpst20 = CategoryProductstyle.create!(name: 'tuxedo shirts', code_name: 'p_style_tuxedo_shirts', category_productsubtype_id: cps10, user_id: us01)
 # clothing/bottoms & trousers
-cpst21 = CategoryProductstyle.create!(name: 'shorts', code_name: 'p_style_shorts', category_productsubtype_id: 0, admin_id: 1)
-cpst22 = CategoryProductstyle.create!(name: 'sweatpants', code_name: 'p_style_shorts', category_productsubtype_id: 0, admin_id: 1)
+cpst21 = CategoryProductstyle.create!(name: 'shorts', code_name: 'p_style_shorts', category_productsubtype_id: cps11, user_id: us01)
+cpst22 = CategoryProductstyle.create!(name: 'sweatpants', code_name: 'p_style_shorts', category_productsubtype_id: cps11, user_id: us01)
 # clothing/jeans
-cpst23 = CategoryProductstyle.create!(name: 'dark wash', code_name: 'p_style_dark_wash', category_productsubtype_id: 0, admin_id: 1)
-cpst24 = CategoryProductstyle.create!(name: 'mid wash', code_name: 'p_style_mid_wash', category_productsubtype_id: 0, admin_id: 1)
+cpst23 = CategoryProductstyle.create!(name: 'dark wash', code_name: 'p_style_dark_wash', category_productsubtype_id: cps12, user_id: us01)
+cpst24 = CategoryProductstyle.create!(name: 'mid wash', code_name: 'p_style_mid_wash', category_productsubtype_id: cps12, user_id: us01)
 # --------------------------------------
 # footwear/boots
-cpst25 = CategoryProductstyle.create!(name: 'casual boots', code_name: 'p_style_casual_boots', category_productsubtype_id: 0, admin_id: 1)
-cpst26 = CategoryProductstyle.create!(name: 'smart boots', code_name: 'p_style_smart_boots', category_productsubtype_id: 0, admin_id: 1)
+cpst25 = CategoryProductstyle.create!(name: 'casual boots', code_name: 'p_style_casual_boots', category_productsubtype_id: cps13, user_id: us01)
+cpst26 = CategoryProductstyle.create!(name: 'smart boots', code_name: 'p_style_smart_boots', category_productsubtype_id: cps13, user_id: us01)
 # footwear/flats
-cpst27 = CategoryProductstyle.create!(name: 'sliders & slippers', code_name: 'p_style_sliders_slippers', category_productsubtype_id: 0, admin_id: 1)
-cpst28 = CategoryProductstyle.create!(name: 'flip flops', code_name: 'p_style_flip_flops', category_productsubtype_id: 0, admin_id: 1)
+cpst27 = CategoryProductstyle.create!(name: 'sliders & slippers', code_name: 'p_style_sliders_slippers', category_productsubtype_id: cps14, user_id: us01)
+cpst28 = CategoryProductstyle.create!(name: 'flip flops', code_name: 'p_style_flip_flops', category_productsubtype_id: cps14, user_id: us01)
 # footwear/shoes
-cpst29 = CategoryProductstyle.create!(name: 'loafers', code_name: 'p_style_loafers', category_productsubtype_id: 0, admin_id: 1)
-cpst30 = CategoryProductstyle.create!(name: 'pumps', code_name: 'p_style_pumps', category_productsubtype_id: 0, admin_id: 1)
+cpst29 = CategoryProductstyle.create!(name: 'loafers', code_name: 'p_style_loafers', category_productsubtype_id: cps15, user_id: us01)
+cpst30 = CategoryProductstyle.create!(name: 'pumps', code_name: 'p_style_pumps', category_productsubtype_id: cps15, user_id: us01)
 # footwear/shoes accessories
-cpst31 = CategoryProductstyle.create!(name: 'shoe decorations', code_name: 'p_style_shoe_decorations', category_productsubtype_id: 0, admin_id: 1)
-cpst32 = CategoryProductstyle.create!(name: 'insoles', code_name: 'p_style_insoles', category_productsubtype_id: 0, admin_id: 1)
+cpst31 = CategoryProductstyle.create!(name: 'shoe decorations', code_name: 'p_style_shoe_decorations', category_productsubtype_id: cps16, user_id: us01)
+cpst32 = CategoryProductstyle.create!(name: 'insoles', code_name: 'p_style_insoles', category_productsubtype_id: cps16, user_id: us01)
 
 
 
