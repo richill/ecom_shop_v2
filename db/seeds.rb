73852bb13cf8fc0a7250065b764bfff44f00b7f2
richill <richill.tamakloe@gmail.com>
2024-01-16 23:13:47 +0000

    Update content

--- db/seeds.rb
+++ db/seeds.rb
@@ -174,10 +174,27 @@ cps00 = CategoryProductsubtype.create!(name: 'office software', code_name: 'p_su
 cps00 = CategoryProductsubtype.create!(name: 'mini PC', code_name: 'p_subtype_mini_pc', category_productitemtype_id: cpit12.id, user_id: us01.id)
 cps00 = CategoryProductsubtype.create!(name: 'computer networking', code_name: 'p_subtype_computer_networking', category_productitemtype_id: cpit12.id, user_id: us01.id)
 cps00 = CategoryProductsubtype.create!(name: 'computer peripherals & components', code_name: 'p_subtype_computer_peripherals', category_productitemtype_id: cpit12.id, user_id: us01.id)
-cps00 = CategoryProductsubtype.create!(name: 'device cleaners', code_name: 'p_subtype_device_cleaners', category_productitemtype_id: 12, admin_id: 1)
-cps00 = CategoryProductsubtype.create!(name: 'office electronics', code_name: 'p_subtype_office_electronic', category_productitemtype_id: 12, admin_id: 1)
+cps00 = CategoryProductsubtype.create!(name: 'device cleaners', code_name: 'p_subtype_device_cleaners', category_productitemtype_id: cpit12.id, user_id: us01.id)
+cps00 = CategoryProductsubtype.create!(name: 'office electronics', code_name: 'p_subtype_office_electronic', category_productitemtype_id: cpit12.id, user_id: us01.id)
 cps00 = CategoryProductsubtype.create!(name: 'industrial computers & accessories', code_name: 'p_subtype_industrial_computers_accessories', category_productitemtype_id: cpit12.id, user_id: us01.id)
 # education & office supplies
+cps00 = CategoryProductsubtype.create!(name: 'pens', code_name: 'p_subtype_pens', category_productitemtype_id: cpit13.id, user_id: us01.id)
+cps00 = CategoryProductsubtype.create!(name: 'pencils', code_name: 'p_subtype_pencils', category_productitemtype_id: cpit13.id, user_id: us01.id)
+cps00 = CategoryProductsubtype.create!(name: 'markers & highlighters', code_name: 'p_subtype_markers_highlighters ', category_productitemtype_id: cpit13.id, user_id: us01.id)
+cps00 = CategoryProductsubtype.create!(name: 'correction supplies', code_name: 'p_subtype_correction_supplies', category_productitemtype_id: cpit13.id, user_id: us01.id)
+cps00 = CategoryProductsubtype.create!(name: 'school & educational supplies', code_name: 'p_subtype_school_educational', category_productitemtype_id: cpit13.id, user_id: us01.id)
+cps00 = CategoryProductsubtype.create!(name: 'notebooks & writing pads', code_name: 'p_subtype_notebook', category_productitemtype_id: cpit13.id, user_id: us01.id)
+cps00 = CategoryProductsubtype.create!(name: 'labels, indexes , stickers & stamps', code_name: 'p_subtype_labels, ', category_productitemtype_id: cpit13.id, user_id: us01.id)
+cps00 = CategoryProductsubtype.create!(name: 'tapes, adhesive & fasteners', code_name: 'p_subtype_tapes_adhesive', category_productitemtype_id: cpit13.id, user_id: us01.id)
+cps00 = CategoryProductsubtype.create!(name: 'books & magazines', code_name: 'p_subtype_books_magazines', category_productitemtype_id: cpit13.id, user_id: us01.id)
+cps00 = CategoryProductsubtype.create!(name: 'desk accessories & organiser', code_name: 'p_subtype_desk_accessories', category_productitemtype_id: cpit13.id, user_id: us01.id)
+cps00 = CategoryProductsubtype.create!(name: 'art supplies', code_name: 'p_subtype_art_supplies', category_productitemtype_id: cpit13.id, user_id: us01.id)
+cps00 = CategoryProductsubtype.create!(name: 'office binding supplies', code_name: 'p_subtype_office_binding', category_productitemtype_id: cpit13.id, user_id: us01.id)
+cps00 = CategoryProductsubtype.create!(name: 'filing products', code_name: 'p_subtype_filing_products', category_productitemtype_id: cpit13.id, user_id: us01.id)
+cps00 = CategoryProductsubtype.create!(name: 'calendars, planners & cards', code_name: 'p_subtype_calendars', category_productitemtype_id: cpit13.id, user_id: us01.id)
+cps00 = CategoryProductsubtype.create!(name: 'mail & shipping supplies', code_name: 'p_subtype_mail_shipping', category_productitemtype_id: cpit13.id, user_id: us01.id)
+cps00 = CategoryProductsubtype.create!(name: 'paper', code_name: 'p_subtype_paper', category_productitemtype_id: cpit13.id, user_id: us01.id)
+cps00 = CategoryProductsubtype.create!(name: 'presentation supplies', code_name: 'p_subtype_presentation_supplies', category_productitemtype_id: cpit13.id, user_id: us01.id)
 # security & protection
 # consumer electronics
 # pet supplies
@@ -205,63 +222,6 @@ cps00 = CategoryProductsubtype.create!(name: 'industrial computers & accessories
 
 
 
-
-
-
-
-# level_04
-CategoryProductstyle.delete_all
-# accessories/belts
-cpst01 = CategoryProductstyle.create!(name: 'buckle', code_name: 'p_style_buckle', category_productsubtype_id: cps01.id, user_id: us01.id)
-cpst02 = CategoryProductstyle.create!(name: 'non buckle', code_name: 'p_style_non_buckle', category_productsubtype_id: cps01.id, user_id: us01.id)
-# accessories/gloves
-cpst03 = CategoryProductstyle.create!(name: 'knitted', code_name: 'p_style_knitted', category_productsubtype_id: cps02.id, user_id: us01.id)
-cpst04 = CategoryProductstyle.create!(name: 'fingerless', code_name: 'p_style_fingerless', category_productsubtype_id: cps02.id, user_id: us01.id)
-# accessories/hats & caps
-cpst05 = CategoryProductstyle.create!(name: 'hats', code_name: 'p_style_mitten', category_productsubtype_id: cps03.id, user_id: us01.id)
-cpst06 = CategoryProductstyle.create!(name: 'caps', code_name: 'p_style_caps', category_productsubtype_id: cps03.id, user_id: us01.id)
-# accessories/hair accessories
-cpst07 = CategoryProductstyle.create!(name: 'headbands', code_name: 'p_style_headbands', category_productsubtype_id: cps04.id, user_id: us01.id)
-cpst08 = CategoryProductstyle.create!(name: 'hair clips', code_name: 'p_style_hair_clips', category_productsubtype_id: cps04.id, user_id: us01.id)
-# --------------------------------------
-# coats & jackets/jackets
-cpst09 = CategoryProductstyle.create!(name: 'puffer jacket', code_name: 'p_style_puffer_jacket', category_productsubtype_id: cps05.id, user_id: us01.id)
-cpst10 = CategoryProductstyle.create!(name: 'parkas', code_name: 'p_style_parkas', category_productsubtype_id: cps05.id, user_id: us01.id)
-# coats & jackets/coats
-cpst11 = CategoryProductstyle.create!(name: 'puffer coats', code_name: 'p_style_puffer_coats', category_productsubtype_id: cps06.id, user_id: us01.id)
-cpst12 = CategoryProductstyle.create!(name: 'tweed coats', code_name: 'p_style_tweed_coats', category_productsubtype_id: cps06.id, user_id: us01.id)
-# coats & jackets/capes
-cpst13 = CategoryProductstyle.create!(name: 'ponchos', code_name: 'p_style_ponchos', category_productsubtype_id: cps07.id, user_id: us01.id)
-cpst14 = CategoryProductstyle.create!(name: 'kimonos', code_name: 'p_style_kimonos', category_productsubtype_id: cps07.id, user_id: us01.id)
-# coats & jackets/gilet
-cpst15 = CategoryProductstyle.create!(name: 'puffer', code_name: 'p_style_puffer', category_productsubtype_id: cps08.id, user_id: us01.id)
-cpst16 = CategoryProductstyle.create!(name: 'teddy', code_name: 'p_style_teddy', category_productsubtype_id: cps08.id, user_id: us01.id)
-# --------------------------------------
-# clothing/dresses
-cpst17 = CategoryProductstyle.create!(name: 'occasion dress', code_name: 'p_style_occasion_dress', category_productsubtype_id: cps09.id, user_id: us01.id)
-cpst18 = CategoryProductstyle.create!(name: 'casual dress', code_name: 'p_style_casual_dress', category_productsubtype_id: cps09.id, user_id: us01.id)
-# clothing/tops & tees
-cpst19 = CategoryProductstyle.create!(name: 'shirts', code_name: 'p_style_shirts', category_productsubtype_id: cps10.id, user_id: us01.id)
-cpst20 = CategoryProductstyle.create!(name: 'tuxedo shirts', code_name: 'p_style_tuxedo_shirts', category_productsubtype_id: cps10.id, user_id: us01.id)
-# clothing/bottoms & trousers
-cpst21 = CategoryProductstyle.create!(name: 'shorts', code_name: 'p_style_shorts', category_productsubtype_id: cps11.id, user_id: us01.id)
-cpst22 = CategoryProductstyle.create!(name: 'sweatpants', code_name: 'p_style_shorts', category_productsubtype_id: cps11.id, user_id: us01.id)
-# clothing/jeans
-cpst23 = CategoryProductstyle.create!(name: 'dark wash', code_name: 'p_style_dark_wash', category_productsubtype_id: cps12.id, user_id: us01.id)
-cpst24 = CategoryProductstyle.create!(name: 'mid wash', code_name: 'p_style_mid_wash', category_productsubtype_id: cps12.id, user_id: us01.id)
-# --------------------------------------
-# footwear/boots
-cpst25 = CategoryProductstyle.create!(name: 'casual boots', code_name: 'p_style_casual_boots', category_productsubtype_id: cps13.id, user_id: us01.id)
-cpst26 = CategoryProductstyle.create!(name: 'smart boots', code_name: 'p_style_smart_boots', category_productsubtype_id: cps13.id, user_id: us01.id)
-# footwear/flats
-cpst27 = CategoryProductstyle.create!(name: 'sliders & slippers', code_name: 'p_style_sliders_slippers', category_productsubtype_id: cps14.id, user_id: us01.id)
-cpst28 = CategoryProductstyle.create!(name: 'flip flops', code_name: 'p_style_flip_flops', category_productsubtype_id: cps14.id, user_id: us01.id)
-# footwear/shoes
-cpst29 = CategoryProductstyle.create!(name: 'loafers', code_name: 'p_style_loafers', category_productsubtype_id: cps15.id, user_id: us01.id)
-cpst30 = CategoryProductstyle.create!(name: 'pumps', code_name: 'p_style_pumps', category_productsubtype_id: cps15.id, user_id: us01.id)
-# footwear/shoes accessories
-cpst31 = CategoryProductstyle.create!(name: 'shoe decorations', code_name: 'p_style_shoe_decorations', category_productsubtype_id: cps16.id, user_id: us01.id)
-cpst32 = CategoryProductstyle.create!(name: 'insoles', code_name: 'p_style_insoles', category_productsubtype_id: cps16.id, user_id: us01.id)
 
 
 
