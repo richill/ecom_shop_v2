4d23bfb15d16deef743aa0ac4f71b63a74581160
richill <richill.tamakloe@gmail.com>
2024-03-01 15:14:32 +0000

    Update content

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,11 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resources)
-    products_path(current_user)
+    if current_user
+      dashboard_user_path(current_user)
+    elsif current_admin
+      dashboard_admin_path(current_admin)
+    end
   end
 
   def after_sign_out_path_for(resource_or_scope)
