e5bcacdbf043cb4a27e3585ee2ec86663df96b5e
richill <richill.tamakloe@gmail.com>
2024-03-01 16:22:59 +0000

    Update content

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_01_154446) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_01_162243) do
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -105,7 +105,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_01_154446) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.string "image"
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
