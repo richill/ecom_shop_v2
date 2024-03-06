7fb00eb32d80a0c48790d423e4bd45252861c775
richill <richill.tamakloe@gmail.com>
2024-03-06 16:06:19 +0000

    Update content

--- app/javascript/controllers/dashboard_controller.js
+++ app/javascript/controllers/dashboard_controller.js
@@ -5,41 +5,33 @@ Chart.register(...registerables);
 export default class extends Controller {
   initialize() {
     console.log("Hello controller")
+
+    const data = [0, 20, 30, 40, 50, 60, 70]
+    const labels = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"]
+
     const ctx = document.getElementById('revenueChart')
 
     new Chart(ctx, {
       type: 'line',
+
       data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: labels,
         datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
+          label: "Revenue $",
+          data: data,
+          borderWidth: 3,
+          fill: true
         }]
       },
+
       options: {
+        plugins: { legend: {display: false}  },
         scales: {
-          y: {
-            beginAtZero: true
-          }
+          x: { grid: {display: false} },
+          y: { border: {dash: [5, 5]}, grid: {color: "#d4f3ef"}, beginAtZero: true },
         }
       }
+
     });
   }
 }
