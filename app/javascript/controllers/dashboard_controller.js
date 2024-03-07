f72670ce0ad722c5d5c1c40c7c2c6fd298b730c1
richill <richill.tamakloe@gmail.com>
2024-03-07 13:51:00 +0000

    Updated content

--- app/javascript/controllers/dashboard_controller.js
+++ app/javascript/controllers/dashboard_controller.js
@@ -3,17 +3,17 @@ import { Chart, registerables } from "chart.js";
 Chart.register(...registerables);
 
 export default class extends Controller {
-  static testing = { revenue: Array }
+  static values = { revenue: Array }
 
   initialize() {
     console.log("Hello controller")
 
     // const data = [0, 20, 30, 40, 50, 60, 70]
-    // const labels = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"]
+    const labels = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"]
 
-
-    const data = this.revenueValue.map((item) => item[1]/100.0)
-    const labels = this.revenueValue.map((item) => item[0])
+    const data = this.revenueValue
+    // const data = this.revenueValue.map((item) => item[1]/100.0)
+    // const labels = this.revenueValue.map((item) => item[0])
     console.log("data----->", data)
 
     const ctx = document.getElementById('revenueChart')
