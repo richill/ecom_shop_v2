c8b7c386ce4dd4ce7af3339fe5a95fe3cf3b6d91
richill <richill.tamakloe@gmail.com>
2024-03-08 14:14:12 +0000

    Updated content

--- app/javascript/controllers/products_controller.js
+++ app/javascript/controllers/products_controller.js
@@ -2,7 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="products"
 export default class extends Controller {
-  connect() {
-    console.log("connected to the products controller")
+  static values = { size: String, product: Object }
+
+  // connect() {
+  //   console.log("connected to the products controller")
+  // }
+
+  addToCart() {
+    console.log("product: ", this.productValue)
+  }
+
+  selectColour(e) {
+    this.colourValue = e.target.value
+    const selectedColourEl = document.getElementById("selected-colour")
+    selectedColourEl.innerText = `Selected Colour: ${this.colourValue}`
   }
 }
