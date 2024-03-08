faecbaa8afa9d8469157e641c372f3d263bb3716
richill <richill.tamakloe@gmail.com>
2024-03-08 14:48:53 +0000

    Updated content

--- app/javascript/controllers/products_controller.js
+++ app/javascript/controllers/products_controller.js
@@ -5,7 +5,21 @@ export default class extends Controller {
   static values = { size: String, product: Object }
 
   addToCart() {
-    console.log("product: ", this.productValue)
+    console.log("product: ", this.productValue);
+
+    const cart = localStorage.getItem("cart");
+    if (cart) {
+
+    } else {
+      const cartArray = []
+      cartArray.push({
+        id: this.productValue.id,
+        name: this.productValue.name,
+        price: this.productValue.price,
+        colour: this.colourValue,
+        quantity: 1
+      })
+    }
   }
 
   selectColour(e) {
