3de857bd0bd69c8a7ab691b24e989c20454551dd
richill <richill.tamakloe@gmail.com>
2024-03-12 21:36:45 +0000

    Updated content

---
+++ app/controllers/webhooks_controller.rb
@@ -0,0 +1,15 @@
+class WebhooksController < ApplicationController
+  skip_forgery_protection
+
+  def stripe
+    # handles all webhook from stripe
+    stripe_secret_key = ENV["ST_ART_STRIPE_API_KEY_TEST"]
+    Stripe.api_key = stripe_secret_key
+    payload = request.body.read
+
+    # the signature key that stripe sends to us and we use the webhook & payload key to verify that is stripe calling our webhook
+    # and now someone else
+    sig_header = request.env["HTT_STRIPE_SIGNATURE"]
+    endpoint_secret = Rails.application.credentials.dig(:stripe, :webhook_secret)
+  end
+end
