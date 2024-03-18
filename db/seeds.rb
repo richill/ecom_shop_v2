d549e3a2fc924ba6ffabb4b2d0d87b21debfa69c
richill <richill.tamakloe@gmail.com>
2024-03-18 22:33:19 +0000

    Updated content

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,11 @@ admin01 = Admin.create!(
   password: "password",
 )
 
+CategoryProducttype.delete_all
+catpt01 = CategoryProducttype.create!(name: "men", code_name: nil,  admin: admin01)
+catpt02 = CategoryProducttype.create!(name: "women", code_name: nil,  admin: admin01)
+catpt03 = CategoryProducttype.create!(name: "products", code_name: nil,  admin: admin01)
+
 CategoryProductitemtype.delete_all
 catpit01 = CategoryProductitemtype.create!(name: "clothing", code_name: nil, admin: admin01)
 catpit02 = CategoryProductitemtype.create!(name: "accessories", code_name: nil, admin: admin01)
@@ -31,10 +36,34 @@ catps05 = CategoryProductstyle.create!(name: "arm-chair", code_name: nil, catego
 catps06 = CategoryProductstyle.create!(name: "shelf", code_name: nil, category_productsubtype_id: catpst03.id,  admin: admin01)
 catps07 = CategoryProductstyle.create!(name: "sofa", code_name: nil, category_productsubtype_id: catpst03.id,  admin: admin01)
 
-CategoryProducttype.delete_all
-catpt001 = CategoryProducttype.create!(name: "men", code_name: nil,  admin: admin01)
-catpt002 = CategoryProducttype.create!(name: "women", code_name: nil,  admin: admin01)
-catpt003 = CategoryProducttype.create!(name: "products", code_name: nil,  admin: admin01)
+Product.delete_all
+prod01 = Product.create!(
+  name: "arm chair",
+  image_url: "https://asset1.cxnmarksandspencer.com/is/image/mands/HF_05_T39_9876P_NC_X_EC_0?wid=640&qlt=80",
+  category_producttype_id: catpt01.id, #product
+  category_productitemtype_id: catpit03.id, #home furnishing
+  category_productsubtype_id: catpst03.id, #furniture
+  category_productstyle_id: catps05.id, #arm-chair
+  description: nil,
+  admin_id: admin01,
+  price: 18
+)
+prod02 = Product.create!(
+  name: "body-suit",
+  image_url: "https://asset1.cxnmarksandspencer.com/is/image/mands/SD_04_T92_4525S_J4_X_EC_0?wid=640&qlt=80",
+  category_producttype_id: catpt01.id, #product
+  category_productitemtype_id: catpit01.id, #clothing
+  category_productsubtype_id: catpst02.id, #bodysuit
+  category_productstyle_id: catps04.id, #casual bodysuit
+  description: nil,
+  admin_id: admin01,
+  price: 18
+)
+
+
+
+OrderProduct.delete_all
+op00 = OrderProduct.create!(product_id: "loreum", order_id: 1, quantity: 4, colour: "black")
 
 
 
