5fc7154aec7ede941f5572099bcb7d08ff33e550
richill <richill.tamakloe@gmail.com>
2024-03-27 20:27:58 +0000

    Updated content

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -15,4 +15,12 @@ class CartItem < ApplicationRecord
                          partial: "carts/item_count",
                          locals: { count: cart.quantity}
   end
+
+  after_destroy_commit do
+    broadcast_remove_to cart
+    broadcast_replace_to cart,
+                         target: "cart_count",
+                         partial: "carts/item_count",
+                         locals: { count: cart.quantity}
+  end
 end
