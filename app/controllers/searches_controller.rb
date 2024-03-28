77a4f6364f2861b83bbcac55ffd2b883740a43b2
richill <richill.tamakloe@gmail.com>
2024-03-28 09:34:23 +0000

    Update content

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,2 +1,12 @@
 class SearchesController < ApplicationController
+  def show
+    @products = ProductSearch.call(params)
+    respond_to do |format|
+      form.turbo_stream {
+        render turbo_stream: turbo_stream.replace(
+
+        )
+      }
+    end
+  end
 end
