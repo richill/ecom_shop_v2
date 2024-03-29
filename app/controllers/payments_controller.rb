b7eef094ebc840b2310a24a21984e798e31ce052
richill <richill.tamakloe@gmail.com>
2024-03-29 14:08:14 +0000

    Update content

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,6 +1,6 @@
 class PaymentsController < ApplicationController
   def show
-    current_cart&.payment_pending! if current_cart&.checkout_pending
+    current_cart&.payment_pending! if current_cart&.checkout_pending?
     cookies.delete(:cart_token)
   end
 end
