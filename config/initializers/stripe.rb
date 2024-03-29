82d069e71cc42e23078140ffa86797f3c3d48329
richill <richill.tamakloe@gmail.com>
2024-03-29 12:59:37 +0000

    Update content

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,8 +1,1 @@
-if Rails.env.development?
-  #development
-  Stripe.api_key = ENV["ST_ART_STRIPE_API_KEY_TEST"]
-  Rails.application.credentials.webhook_secret = ENV["ST_ART_STRIPE_WEBHOOK_SECRET_TEST"]
-elsif Rails.env.production?
-  # production
-  Stripe.api_key = ENV["ST_ART_STRIPE_API_KEY"]
-end
+Stripe.api_key = Rails.application.credentials.dig(:stripe, :publishable_key)
